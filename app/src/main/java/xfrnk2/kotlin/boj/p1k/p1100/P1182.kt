95fcd592ede4088b84e85db46b7196d50e436319
xfrnk2 <xfrnk2@gmail.com>
2023-01-26 22:56:10 +0900

    init

---
+++ app/src/main/java/xfrnk2/kotlin/boj/p1k/p1100/P1182.kt
@@ -0,0 +1,26 @@
+package xfrnk2.kotlin.boj.p1k.p1100
+
+
+fun main(args: Array<String>) {
+    val (N, S) = readln().split(" ").map { it.toInt() }
+    val list = readln().split(" ").map { it.toInt() }
+
+    val size = 1 shl N
+    var ans = 0
+
+
+    for (s in 1 until size) {
+        var sum = 0;
+        for (i in 0 until N) {
+            if ((s and (1 shl i)) != 0) {
+                sum += list[i]
+            }
+        }
+        if (sum == S) {
+            ans++
+        }
+
+    }
+    print(ans)
+
+}
